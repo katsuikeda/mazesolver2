ff8b0b78a58ef30fd331c8d57a7587042945c191
katsuikeda <katsuyukiikeda142857@gmail.com>
2024-11-16 03:06:20 -0800

    Implement line drawing functionality in graphics

--- graphics.py
+++ graphics.py
@@ -5,10 +5,10 @@ class Window:
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title("Maze Solver")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
         self.__canvas = Canvas(self.__root, width=width, height=height, bg="white")
         self.__canvas.grid(column=0, row=0, sticky=(N, W, E, S))
         self.__is_running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
         self.__root.update_idletasks()
@@ -22,3 +22,28 @@ class Window:
 
     def close(self):
         self.__is_running = False
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(
+            self.p1.x,
+            self.p1.y,
+            self.p2.x,
+            self.p2.y,
+            fill=fill_color,
+            width=2,
+        )
--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from graphics import Window
+from graphics import Window, Point, Line
 
 
 def main():
     win = Window(800, 600)
+    p1 = Point(50, 80)
+    p2 = Point(500, 500)
+    p3 = Point(50, 520)
+    p4 = Point(500, 100)
+    line = Line(p1, p2)
+    line2 = Line(p3, p4)
+    win.draw_line(line, "black")
+    win.draw_line(line2, "black")
+
     win.wait_for_close()
 
 
