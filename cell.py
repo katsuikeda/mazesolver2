5fdbe1ec57c182609e037aa4a802f621eb5ecc30
katsuikeda <katsuyukiikeda142857@gmail.com>
2024-11-17 00:38:06 -0800

    Refactor Cell class to make Window parameter optional and update drawing logic to handle None case

--- cell.py
+++ cell.py
@@ -5,7 +5,6 @@ class Cell:
     # (x1, y1): top-left, (x2, y2): bottom-right
     def __init__(
         self,
-        win,
         x1,
         y1,
         x2,
@@ -14,8 +13,8 @@ class Cell:
         has_right_wall=True,
         has_top_wall=True,
         has_bottom_wall=True,
+        win=None,
     ):
-        self._win = win
         self._x1 = x1
         self._y1 = y1
         self._x2 = x2
@@ -24,8 +23,11 @@ class Cell:
         self.has_right_wall = has_right_wall
         self.has_top_wall = has_top_wall
         self.has_bottom_wall = has_bottom_wall
+        self._win = win
 
     def draw(self):
+        if self._win is None:
+            return
         if self.has_left_wall:
             left_wall = Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
             self._win.draw_line(left_wall)
@@ -44,7 +46,9 @@ class Cell:
             Point((self._x1 + self._x2) / 2, (self._y1 + self._y2) / 2),
             Point((to_cell._x1 + to_cell._x2) / 2, (to_cell._y1 + to_cell._y2) / 2),
         )
+        fill_color = "red"
         if undo:
-            self._win.draw_line(line, fill_color="grey")
+            fill_color = "grey"
+            self._win.draw_line(line, fill_color)
             return
-        self._win.draw_line(line, fill_color="red")
+        self._win.draw_line(line, fill_color)
